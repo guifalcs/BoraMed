2e6ab383beb13f79c97b1e91204ffb21b0c1d388
Guilherme Falcao <guilherme.falcao@supporthealth.com.br>
2026-05-27 08:58:03 -0300

    sentry dns

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseUrl: 'http://127.0.0.1:54321',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
-  sentryDsn: '',
+  sentryDsn: 'https://52153d44379bda28bed8d9a2868f260b@o4511458808561664.ingest.us.sentry.io/4511458868920320',
 };
